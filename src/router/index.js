d807562d49d4f9635bb680fa29a275b1158e654b
Konstantin Popov <zamposad@gmail.com>
2023-08-12 20:26:35 +0300

    add structure

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,23 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'about',
+      component: () => import('../views/About.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: '/projects',
+      name: 'projects',
+      component: () => import('../views/Projects.vue')
+    },
+    {
+      path: '/photos',
+      name: 'photos',
+      component: () => import('../views/Photos.vue')
+    },
+    {
+      path: 'links',
+      name: 'links',
+      component: () => import('../views/Links.vue')
     }
   ]
 })
